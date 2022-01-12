c4400333ed363b59df5cbabf00f97d61d3920833
Jose Rosenaldo da S Junior <joserosenaldo@beltsistemas.com.br>
2022-01-12 20:10:48 -0300

    Ajuste de Titulo

--- web/src/services/utils.js
+++ web/src/services/utils.js
@@ -1,10 +1,10 @@
 export default {
     baseURL:
       process.env.NODE_ENV == 'production'
-        ? 'https://ws.salaonamao.com.br:8000'
+        ? 'https://ws.systudio.com.br:8000'
         : 'http://localhost:8000',
     AWS: {
-      bucketURL: 'https://salao-na-mao.s3.amazonaws.com',
+      bucketURL: 'https://systudio.s3.amazonaws.com',
     },
     validateEmail: (email) => {
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
