68946914cb4b0a7b49899cac1844d2a988e89679
Jose Rosenaldo da S Junior <joserosenaldo@beltsistemas.com.br>
2022-01-11 21:54:53 -0300

    Painel-Usuario

---
+++ web/src/util.js
@@ -0,0 +1,7 @@
+export default {
+    hourToMinutes: (hourMinute) => {
+      const [hour, minutes] = hourMinute.split(':');
+      return parseInt(parseInt(hour) * 60 + parseInt(minutes));
+    },
+  };
+  
